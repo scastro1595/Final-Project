081a6800bebb8c52cbdbf9d4f17adaeeb0ca4802
scastro1595 <stephaniecastro2013@gmail.com>
2025-05-01 12:20:35 -0500

    Update employer.py

--- employer.py
+++ employer.py
@@ -1,29 +1,25 @@
 # employer.py
 # Employer class: register, login, post jobs, delete jobs, view applicants
 
-
+from job import Job
 
 class Employer:
     def __init__(self, company_name, email, password):
         self.company_name = company_name
         self.email = email
         self.password = password
-        self.posted_jobs = []  # List of Job objects posted
-        self.messages = []  # Inbox messages
+        self.posted_jobs = []  # List of Job objects
 
-    def post_job(self, job):
-        self.posted_jobs.append(job)
+    def post_job(self, title, location, description, required_skills):
+        new_job = Job(title, self.company_name, location, description, required_skills)
+        self.posted_jobs.append(new_job)
+        return new_job
 
-    def delete_job(self, job_id):
-        self.posted_jobs = [job for job in self.posted_jobs if job.job_id != job_id]
+    def delete_job(self, job_to_delete):
+        if job_to_delete in self.posted_jobs:
+            self.posted_jobs.remove(job_to_delete)
+            return True
+        return False
 
-    def view_applicants(self, applications):
-        # Filter applications related to this employer's jobs
-        my_job_ids = [job.job_id for job in self.posted_jobs]
-        return [app for app in applications if app.job_id in my_job_ids]
-
-    def receive_message(self, message):
-        self.messages.append(message)
-
-    def view_inbox(self):
-        return self.messages
+    def view_posted_jobs(self):
+        return self.posted_jobs
