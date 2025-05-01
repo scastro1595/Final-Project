2e666d40f604d37de08d688a0f0d5707900fce1d
scastro1595 <stephaniecastro2013@gmail.com>
2025-05-01 12:21:54 -0500

    Update message.py

--- message.py
+++ message.py
@@ -9,3 +9,13 @@ class Message:
         self.receiver_email = receiver_email
         self.content = content
 
+    def display(self):
+        return f"From: {self.sender_email}\nTo: {self.receiver_email}\nMessage: {self.content}"
+
+
+def save_messages_to_file():
+    return None
+
+
+def load_messages_from_file():
+    return None
